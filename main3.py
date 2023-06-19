a434653b2766289e699450011a366f825dd540d2
BasMan <s.laget@scarlet.be>
2023-06-19 20:37:48 +0200

    removed car project.

--- main3.py
+++
@@ -1,8 +0,0 @@
-import components
-import time
-
-meter = components.VoltageMeter(pin=34, voltage_devider=True)
-
-while True:
-    print(meter.read())
-    time.sleep(1)
--- test.py
+++
@@ -1,8 +0,0 @@
-import time
-
-wait_time = time.ticks_ms()
-
-while True:
-    if wait_time + 1000 < time.ticks_ms():
-        wait_time = time.ticks_ms()
-        print("test")
